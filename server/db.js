ec69a94b8d46a37550e6cd3003dda2747da2ccbe
Vinit Amar <123@gmail.com>
2026-03-09 03:19:10 +0000

    Database connected to server

--- server/db.js
+++ server/db.js
@@ -1,1 +1,10 @@
-const Pool = require ("pg").Pool;
+const Pool = require("pg").Pool;
+
+const Pool = new Pool({
+    user: "postgres",
+    password: "Vuntu123",
+    port: 5432,
+    host: "localhost",
+    database: "perntodo"
+});
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
-const cors =require("cors");
+const pool = require("./db");
+const cors = require("cors");
 
 //middleware
 app.use(cors());
 app.use(express.json());
+//ROUTES//
+//CREATE A TODO
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
